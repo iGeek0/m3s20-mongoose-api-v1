bd6ee23026b996ec8f2251a8054a9a441970148f
iGeek <jesus.cardenas.d@gmail.com>
2023-06-22 08:53:42 -0600

    ajuste perfil

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -94,29 +94,21 @@ const perfilInfo = async (req = request, res = response) => {
     });
 }
 
-const changePassword = async (req = request, res = response) => {
+const editUser = async (req = request, res = response) => {
     try {
-        const { old_password, password } = req.body;
+        const body = req.body;
         const tokenDecoded = leerToken(req.headers.authorization.split(" ")[1]);
         const { id } = tokenDecoded.data;
-        let usuario = await Usuario.findById(id);
-        const isValidPassword = await comparePassword(old_password, usuario.password);
-
-        if (!isValidPassword) {
-            res.status(400).json({
-                msg: "Password anterior incorrecto",
-                detalle: null
-            });
+        if (body.password) {
+            body.password = await hashPassword(body.password);
         } else {
-
-            usuario.password = await hashPassword(password);
-            await usuario.save();
-
-            res.status(201).json({
-                msg: "El password se cambio correctamente",
-                detalle: usuario
-            });
+            delete body.password;
         }
+        await Usuario.findByIdAndUpdate(id, body);
+        res.status(200).json({
+            msg: "Usuario actualizado correctamente",
+            detalle: Usuario
+        });
     } catch (error) {
         res.status(400).json({
             msg: "No se pudo cambiar el password",
@@ -132,5 +124,5 @@ module.exports = {
     logIn,
     validarTokenPost,
     perfilInfo,
-    changePassword
+    editUser
 }
--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-const {usuariosGet, signUp, logIn, validarTokenPost, perfilInfo, changePassword} = require('../controllers/usuarios.controller');
+const {usuariosGet, signUp, logIn, validarTokenPost, perfilInfo, editUser} = require('../controllers/usuarios.controller');
 const chkToken =  require('../middlewares/auth.middleware');
 
 router.get("/usuarios", usuariosGet);
@@ -8,7 +8,7 @@ router.post("/signup", signUp);
 router.post("/login", logIn);
 router.post("/validar_token", validarTokenPost);
 router.get("/perfil",chkToken, perfilInfo);
-router.put("/change_password", chkToken, changePassword);
+router.put("/edit_user", chkToken, editUser);
 
 
 
